e093aedcb37d56c32fd7227cab118f30b4789be3
priyanshu jain <68162762+priyanshu003@users.noreply.github.com>
2021-02-24 09:13:00 +0530

    Create NQueens.java

---
+++ NQueens.java
@@ -0,0 +1,58 @@
+/*1. You are given a number n, the size of a chess board.
+2. You are required to place n number of queens in the n * n cells of board such that no queen can kill another.
+Note - Queens kill at distance in all 8 directions
+3. Complete the body of printNQueens function - without changing signature - to calculate and print all safe configurations of n-queens. Use sample input and output to get more idea.
+*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] chess = new int[n][n];
+        printNQueens(chess, "", 0);
+    }
+
+    public static void printNQueens(int[][] chess, String qsf, int row){
+        if(row == chess.length){
+            System.out.println(qsf + ".");
+            return;
+        }
+        for(int col = 0; col < chess.length; col++){
+            if(chess[row][col] == 0 && isQueenSafe(chess, row, col) == true){
+                chess[row][col] = 1;
+                printNQueens(chess, qsf + row + "-" + col + ", ", row + 1);
+                chess[row][col] = 0;
+            }
+        }
+    }
+
+    public static boolean isQueenSafe(int[][] chess, int row, int col){
+        for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--){
+            if(chess[i][j] == 1){
+                return false;
+            }
+        }
+
+        for(int i = row - 1, j = col; i >= 0; i--){
+            if(chess[i][j] == 1){
+                return false;
+            }
+        }
+
+        for(int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++){
+            if(chess[i][j] == 1){
+                return false;
+            }
+        }
+
+        // for(int i = row, j = col - 1; j >= 0; j--){
+        //     if(chess[i][j] == 1){
+        //         return false;
+        //     }
+        // }
+
+        return true;
+    }
